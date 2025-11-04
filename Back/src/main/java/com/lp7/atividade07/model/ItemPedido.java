94fadb68e330ec73e9480dfdbd793ecc7f8dbfa9
Pedro-Spindola <spindolapedrohenrique99@gmail.com>
2025-11-03 23:39:51 -0300

    setup inicial do backend.

---
+++ Back/src/main/java/com/lp7/atividade07/model/ItemPedido.java
@@ -0,0 +1,10 @@
+package com.lp7.atividade07.model;
+
+import java.math.BigDecimal;
+
+public class ItemPedido {
+    private Long id;
+    private Produto produto;
+    private Integer quantidade;
+    private BigDecimal subTotal;
+}
